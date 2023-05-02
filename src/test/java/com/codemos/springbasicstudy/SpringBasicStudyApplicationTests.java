2e17ff4312bc3bc76147c2129d80d53007b7146f
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-03 00:07:20 +0900

    Spring Application Test

---
+++ src/test/java/com/codemos/springbasicstudy/SpringBasicStudyApplicationTests.java
@@ -0,0 +1,11 @@
+package com.codemos.springbasicstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicStudyApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
