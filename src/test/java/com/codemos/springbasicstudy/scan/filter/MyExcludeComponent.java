8866e1ea33be807b289c11584c0fe73c21780bb1
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-02 23:56:47 +0900

    Bean Exclusion Annotation

---
+++ src/test/java/com/codemos/springbasicstudy/scan/filter/MyExcludeComponent.java
@@ -0,0 +1,9 @@
+package com.codemos.springbasicstudy.scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcludeComponent {
+}
