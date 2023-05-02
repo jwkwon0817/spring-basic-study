6856deb7b0fdbdaf0289247525cb068a29e77fcd
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-03 00:07:25 +0900

    Spring Application

---
+++ src/main/java/com/codemos/springbasicstudy/SpringBasicStudyApplication.java
@@ -0,0 +1,11 @@
+package com.codemos.springbasicstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicStudyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicStudyApplication.class, args);
+	}
+}
