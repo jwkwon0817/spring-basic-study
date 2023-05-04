47bc2863fc1328e3b994159b4b98541b26a1dd59
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-04 23:05:06 +0900

    Singleton with Prototype Test 1

--- src/test/java/com/codemos/springbasicstudy/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/com/codemos/springbasicstudy/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package com.codemos.springbasicstudy.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -33,20 +34,17 @@ public class SingletonWithPrototypeTest1 {
 		
 		ClientBean clientBean2 = ac.getBean(ClientBean.class);
 		int count2 = clientBean2.logic();
-		assertThat(count2).isEqualTo(2);
+		assertThat(count2).isEqualTo(1);
 	}
 	
 	@Scope("singleton")
 	@Configuration
 	static class ClientBean {
-		private final PrototypeBean prototypeBean;
-		
 		@Autowired
-		public ClientBean(PrototypeBean prototypeBean) {
-			this.prototypeBean = prototypeBean;
-		}
+		private Provider<PrototypeBean> prototypeBeansProvider;
 		
 		public int logic() {
+			PrototypeBean prototypeBean = prototypeBeansProvider.get();
 			prototypeBean.addCount();
 			return prototypeBean.getCount();
 		}
