ea621a13ec5a7d5f1a34e5675d3a2b55da308b63
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2023-05-05 14:42:28 +0900

    Log Demo Controller

--- src/main/java/com/codemos/springbasicstudy/web/LogDemoController.java
+++ src/main/java/com/codemos/springbasicstudy/web/LogDemoController.java
@@ -1,7 +1,29 @@
 package com.codemos.springbasicstudy.web;
 
+import com.codemos.springbasicstudy.common.MyLogger;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequiredArgsConstructor
 public class LogDemoController {
+	private final LogDemoService logDemoService;
+	private final MyLogger myLogger;
+	
+	@RequestMapping("log-demo")
+	@ResponseBody
+	public String logDemo(HttpServletRequest request) throws InterruptedException {
+		String requestURL = request.getRequestURL().toString();
+		
+		System.out.println("myLogger = " + myLogger.getClass());
+		myLogger.setRequestUrl(requestURL);
+		
+		myLogger.log("controller test");
+		Thread.sleep(1000);
+		logDemoService.logic("testId");
+		return "OK";
+	}
 }
